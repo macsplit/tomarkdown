34574b5a2a06312f773193623c8dfcebc0416313
Lee Hanken <github-12-2017-ds8@leehanken.uk>
2022-04-17 20:26:42 +0100

    centred layout on iPhone, made a switch for links rather than button, embedded JS and CSS to avoid Cross Site issues

--- ToMarkdown Extension/Resources/popup.js
+++ ToMarkdown Extension/Resources/popup.js
@@ -1,7 +1,8 @@
 console.log("popup.js")
 document.querySelector("#convertButton1").addEventListener('click', ()=> {
     document.querySelector("#convertButton1").classList.add("convertPressed");
-    browser.runtime.sendMessage("convert1").then((response) => {
+    let message = (document.getElementById('includeLinks').checked) ? "convert1" : "convert2";
+    browser.runtime.sendMessage(message).then((response) => {
         console.log("response: ", response);
         setTimeout(()=>{
             document.querySelector("#convertButton1").classList.remove("convertPressed");
@@ -9,13 +10,4 @@ document.querySelector("#convertButton1").addEventListener('click', ()=> {
         },250);
     });
 },false);
-document.querySelector("#convertButton2").addEventListener('click', ()=> {
-    document.querySelector("#convertButton2").classList.add("convertPressed");
-    browser.runtime.sendMessage("convert2").then((response) => {
-        console.log("response: ", response);
-        setTimeout(()=>{
-            document.querySelector("#convertButton2").classList.remove("convertPressed");
-            window.close();
-        },250);
-    });
-},false);
+
